1f20c248b4eb18f3391e5824068e5fef33a1f16f
Nicu2002 <nicu.mercurial@bk.ru>
2022-05-12 18:55:21 +0300

    translated on hooks App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useEffect, useState} from 'react';
 import nextId from "react-id-generator";
 import './app.css';
 import APIService from "../../services/APIService";
@@ -7,37 +7,116 @@ import ItemsList from '../itemsList/ItemsList';
 import AddForm from '../addForm/AddForm';
 import Filter from '../filter/Filter'
 
-class App extends Component {
-  state = {
-    data: [],
-    term: '',
-    dataLoad: true
+// class App extends Component {
+//   state = {
+//     data: [],
+//     term: '',
+//     dataLoad: true
+//   }
+
+//   getData = new APIService();
+
+//   componentDidMount() {
+
+//     this.getData.getAllData()
+//       .then(this.onDataLoaded);
+//   }
+
+//   onDataLoaded = (newItems)=> {
+//     this.setState(() => ({
+//       data: [...newItems],
+//       dataLoad : false
+//     }))
+//   }
+
+//   onDeleteItem = (id) => {
+//     this.setState(({ data }) => {
+//       return {
+//         data: data.filter(item => item.id !==id)
+//       }
+//     })
+//   }
+
+//   onFilter = (term, items) => {
+//     if (term.length === 0) {
+//       return items;
+//     }
+//     return items.filter(item => {
+//       return item.name.indexOf(term) > -1
+//     });
+//   }
+
+//   onChangeTerm = (term) => {
+//     this.setState(() => ({ term }));
+//     this.getData.getDataByName(term)
+//       .then(this.onDataLoaded);
+//   }
+  
+//   idGenerator = nextId('add-');
+
+//   addItem = ({ job, city, name, email, dateCreated, phoneNumber }) => {
+//     const item = { 
+//       id: this.idGenerator,
+//       job: job,
+//       city: city,
+//       name: name,
+//       email: email,
+//       dateCreated: dateCreated,
+//       phoneNumber: phoneNumber
+//     };
+//     const result = { // on server keys are with uppercase
+//       id: item.id,
+//       Job: job,
+//       City: city,
+//       Name: name,
+//       Email: email,
+//       DateCreated: dateCreated,
+//       "Phone Number": phoneNumber
+//     };
+//     this.getData.postData(result);
+//     const newState = [...this.state.data, item];
+//     this.setState(() => ({
+//       data: newState
+//     }))
+//     this.idGenerator = nextId("add-");
+//   }
+
+//   render() {
+//     const visibleData = this.state.data;
+//     return (
+//       <div className='app'>
+//         <Filter onChangeTerm={this.onChangeTerm}/>
+//         <div className='d-flex'>
+//           <ItemsList data={visibleData} onDeleteItem={this.onDeleteItem} loading={this.state.dataLoad}/>
+//           <AddForm addItem={this.addItem} />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [term, setTerm] = useState('');
+  const [dataLoad, setDataLoad] = useState(true);
+
+  let getData = new APIService();
+
+  useEffect(() => {
+    getData.getAllData()
+      .then(onDataLoaded);
+  }, []);
+
+  function onDataLoaded(newItems) {
+    setData([...newItems]);
+    setDataLoad(false);
   }
 
-  getData = new APIService();
-
-  componentDidMount() {
-
-    this.getData.getAllData()
-      .then(this.onDataLoaded);
+  function onDeleteItem(id) {
+    setData(data.filter(item => item.id !== id));
   }
 
-  onDataLoaded = (newItems)=> {
-    this.setState(() => ({
-      data: [...newItems],
-      dataLoad : false
-    }))
-  }
-
-  onDeleteItem = (id) => {
-    this.setState(({ data }) => {
-      return {
-        data: data.filter(item => item.id !==id)
-      }
-    })
-  }
-
-  onFilter = (term, items) => {
+  function onFilter(term, items){
     if (term.length === 0) {
       return items;
     }
@@ -46,17 +125,17 @@ class App extends Component {
     });
   }
 
-  onChangeTerm = (term) => {
-    this.setState(() => ({ term }));
-    this.getData.getDataByName(term)
-      .then(this.onDataLoaded);
+  function onChangeTerm(term) {
+    setTerm(term);
+    getData.getDataByName(term)
+      .then(onDataLoaded);
   }
   
-  idGenerator = nextId('add-');
+  let idGenerator = nextId('add-');
 
-  addItem = ({ job, city, name, email, dateCreated, phoneNumber }) => {
+  function addItem({ job, city, name, email, dateCreated, phoneNumber }) {
     const item = { 
-      id: this.idGenerator,
+      id: idGenerator,
       job: job,
       city: city,
       name: name,
@@ -73,26 +152,21 @@ class App extends Component {
       DateCreated: dateCreated,
       "Phone Number": phoneNumber
     };
-    this.getData.postData(result);
-    const newState = [...this.state.data, item];
-    this.setState(() => ({
-      data: newState
-    }))
-    this.idGenerator = nextId("add-");
+    getData.postData(result);
+    const newState = [...data, item];
+    setData(newState);
+    idGenerator = nextId("add-");
   }
-
-  render() {
-    const visibleData = this.state.data;
-    return (
-      <div className='app'>
-        <Filter onChangeTerm={this.onChangeTerm}/>
-        <div className='d-flex'>
-          <ItemsList data={visibleData} onDeleteItem={this.onDeleteItem} loading={this.state.dataLoad}/>
-          <AddForm addItem={this.addItem} />
-        </div>
+  const visibleData = data;
+  return (
+    <div className='app'>
+      <Filter onChangeTerm={onChangeTerm}/>
+      <div className='d-flex'>
+        <ItemsList data={visibleData} onDeleteItem={onDeleteItem} loading={dataLoad}/>
+        <AddForm addItem={addItem} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
